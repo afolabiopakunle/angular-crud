8e9d2b1b6d84010e1c0b52a846e6f48314c20dfd
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-04-18 17:21:31 +0100

    ready for routing

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -11,33 +11,34 @@ export class ListEmployeesComponent implements OnInit {
   employees: Employee[] = [
     {
       id: 1,
-      name: 'Mary',
+      name: 'Mary Akonumah',
       gender: 'Female',
       contactPreference: 'Email',
+      phoneNumber: 2348054982155,
       dateOfBirth: new Date('10/23/1988'),
-      department: 'IT',
+      department: 'HR',
       isActive: true,
       photoPath: 'assets/images/mary.png',
     },
     {
       id: 2,
-      name: 'Afolabi',
+      name: 'Afolabi Opakunle',
       gender: 'Male',
       contactPreference: 'Phone',
       phoneNumber: 2348030810384,
-      department: 'HR',
+      department: 'ICT',
       isActive: true,
       dateOfBirth: new Date('09/11/1985'),
       photoPath: 'assets/images/mark.png'
     },
     {
       id: 3,
-      name: 'John',
+      name: 'John Otabil',
       gender: 'Male',
       contactPreference: 'Phone',
       phoneNumber: 345204020040,
       dateOfBirth: new Date('2/24/1998'),
-      department: 'IT',
+      department: 'Finance',
       isActive: false,
       photoPath: 'assets/images/john.png'
     }
